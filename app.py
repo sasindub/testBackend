047e9b25fee63f1a65619658ed96403358d6e425
Sasindu Bandara <sasindu.bandara@innovative-e.com>
2024-09-18 17:38:32 +0530

    updated

--- app.py
+++ app.py
@@ -39,10 +39,10 @@ import joblib
 import numpy as np
 
 # Resolve the absolute path of the .pkl file
-model_path = "none"
+model_path = os.path.join(os.path.dirname(__file__), "vaccine_demand_model.pkl")
 
 # Load the trained model
-model = joblib.load(model_path)
+#model = joblib.load(model_path)
 
 def predict_demand(region, month, population, prev_demand):
     # X = np.array([[month, region, population, prev_demand]])
