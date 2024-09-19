50c5a7992196b34467515065d9fff091556cf899
Sasindu Bandara <sasindu.bandara@innovative-e.com>
2024-09-19 10:09:19 +0530

    updated

--- model/predict_model.py
+++ model/predict_model.py
@@ -8,7 +8,7 @@ model_path = os.path.join(BASE_DIR, "vaccine_demand_model.pkl")
 # Load the trained model
 #model = joblib.load(model_path)
 
-def predict_demand(region, month, population, prev_demand):
+def predict_demand():
     # X = np.array([[month, region, population, prev_demand]])
     # prediction = model.predict(X)
     # return int(np.round(prediction[0]))
@@ -16,3 +16,5 @@ def predict_demand(region, month, population, prev_demand):
 
 # def test():
 #     return model_path
+
+#region, month, population, prev_demand
