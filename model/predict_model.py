07bc01b8125c998706659db413acf09950fb1e4d
Sasindu Bandara <sasindu.bandara@innovative-e.com>
2024-09-21 02:14:34 +0530

    updated

--- model/predict_model.py
+++ model/predict_model.py
@@ -1,12 +1,12 @@
 import os
-import joblib
+import pickle
 import numpy as np
 
 # Resolve the absolute path of the .pkl file
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(BASE_DIR, "vaccine_demand_model.pkl")
 # Load the trained model
-joblib.load(model_path)
+model = pickle.load(open(model_path, "rb"))
 
 def predict_demand():
     # X = np.array([[month, region, population, prev_demand]])
