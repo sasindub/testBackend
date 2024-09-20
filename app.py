f0fa268d03eb2899c11174624a764892feb15a15
Sasindu Bandara <sasindu.bandara@innovative-e.com>
2024-09-21 03:32:52 +0530

    new updated

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from model.predict_model import predict_demand
+#from model.predict_model import predict_demand
 import pandas as pd
 from typing import List
 
@@ -35,15 +35,35 @@ class DataPoint(BaseModel):
     value: float
 
 import pickle
-# Home route to render the HTML page
-@app.get("/")
-async def home(request: Request):   
-    # Create a sample object
+import os
+import pickle
+import numpy as np
+import pickle
+
+# # Resolve the absolute path of the .pkl file
+# # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# # model_path = os.path.join(BASE_DIR, "vaccine_demand_model.pkl")
+# # Load the trained model
+# # model = pickle.load(open(model_path, "rb"))
+
+def predict_demand():
+ # Create a sample object
     data = {"key1": "value1", "key2": 42}
 
     # Save the object to a new .pkl file
     with open("new_model.pkl", "wb") as f:
         pickle.dump(data, f)
+
+        # Resolve the absolute path of the .pkl file
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(BASE_DIR, "new_model.pkl")
+    # Load the trained model
+    #model = pickle.load(open(model_path, "rb"))
+    return model_path
+
+# Home route to render the HTML page
+@app.get("/")
+async def home(request: Request):   
     return {"message": predict_demand()}
 
 # Prediction route to accept JSON request body
