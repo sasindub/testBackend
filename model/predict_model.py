b4ee6c8f1b30255ed3556af10bcb6334454fa679
Sasindu Bandara <sasindu.bandara@innovative-e.com>
2024-09-21 04:07:08 +0530

    new update model

--- model/predict_model.py
+++ model/predict_model.py
@@ -9,7 +9,7 @@ model_path = os.path.join(BASE_DIR, "vaccine_demand_model.pkl")
 # Load the trained model #
 model = pickle.load(open("/tmp/8dcd9c3dc14c1d7/model/vaccine_demand_model.pkl", "rb"))
 
-def predict_demand():
+def predict_demand(region, month, population, prev_demand):
     X = np.array([[month, region, population, prev_demand]])
     prediction = model.predict(X)
     return int(np.round(prediction[0]))
